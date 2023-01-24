c404fd51b144947d9498fdcb985d33cd06aa40c5
thinhle93 <35715420+thinhle93@users.noreply.github.com>
2023-01-24 12:36:44 -0800

    Centered grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./style.css";
-import Memory from './Memory'
+import React from 'react';
+import './style.css';
+import Memory from './Memory';
 
 export default function App() {
   return (
-    <div>
+    <div className="center">
       <Memory />
     </div>
   );
